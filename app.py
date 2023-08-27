b43165a471e4c024a88f1de2a91e4f2e81674ab5
heidless <rob.lockhart@yahoo.co.uk>
2023-08-27 09:48:00 +0100

    first

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def base_url():
     """Base url to test API."""
 
     response = {
-        'response': 'Hello world!'
+        'response': '*** Hello world! ***'
     }
 
     return jsonify(response)
